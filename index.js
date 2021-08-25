b8182392304e6b53c6842a36d0c312b32cf41fe5
Andy2472 <89529008+Andy2472@users.noreply.github.com>
2021-08-25 09:01:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+/* var saludo = prompt ("Hola, buen día, dime tu nombre");
+
+alert("Hola " + saludo + " Bienvenido a mi página Web") */
+
+const profe = "Jorge Esneider"
+var nombreprofe = prompt ("¿Hola, cual es tu nombre?")
+
+if ( nombreprofe == profe){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+
+else if ( nombreprofe == "Jorge"){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+
+else if ( nombreprofe == "jorge"){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+
+else if ( nombreprofe == "JORGE ESNEIDER"){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+
+else if ( nombreprofe == "jorge esneider"){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+else if ( nombreprofe == "jorge Esneider"){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+else if ( nombreprofe == "Jorge esneider"){
+    alert("Hola Profe, bienvenido, espero te parezca agradable mi página")
+}
+
+else if ( nombreprofe == "Esneider"){
+    alert("Hola Profe bienvenido, espero te parezca agradable mi página")
+}
+else if ( nombreprofe == "esneider"){
+    alert("Hola Profe bienvenido, espero te parezca agradable mi página")
+}
+
+else {
+    alert("Hola compañer@ " + nombreprofe + " bienvenid@, espero te encuentres bien y te parezca agradable mi página")
+}
+
+
+
+
+
+
+
+
+
+
+
+
